cc16fd966f67dc9ba16ce461a90f4a8854043ccc
Norsang Nyandak <TNorsang>
2023-09-18 18:21:25 -0400

    Finished the logic! Only some updates left

--- app/src/main/java/com/example/wordle/MainActivity.kt
+++ app/src/main/java/com/example/wordle/MainActivity.kt
@@ -6,6 +6,11 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.text.SpannableStringBuilder
+import android.text.Spannable
+import android.text.style.ForegroundColorSpan
+import android.graphics.Color
+
 
 import com.example.wordle.FourLetterWordLists
 class MainActivity : AppCompatActivity() {
@@ -15,7 +20,6 @@ class MainActivity : AppCompatActivity() {
 
     private val fourLetterWordsList = FourLetterWordLists
     private val wordToGuess = fourLetterWordsList.getRandomFourLetterWord()
-    private var answer : String = ""
 
     private val maxLetters = 4
     private var clickedLetters = StringBuilder()
@@ -33,7 +37,8 @@ class MainActivity : AppCompatActivity() {
 
         guessButton.setOnClickListener{
             if (remainingChances > 0) {
-                answer = checkGuess(clickedLetters)
+                Log.d("MainActivity", "Answer: $wordToGuess")
+                var answer = checkGuess(clickedLetters)
                 Log.d("MainActivity", "Answer: $answer")
                 remainingChances-- // Decrease the remaining chances
                 clickedLetters.setLength(0)
@@ -54,7 +59,14 @@ class MainActivity : AppCompatActivity() {
 
             if (clickedLetters.length < maxLetters){
                 clickedLetters.append(letterClicked)
-                firstWordDisplay.text = clickedLetters.toString()
+                if (remainingChances == 3){
+                    firstWordDisplay.text = clickedLetters.toString()
+                } else if (remainingChances == 2){
+                    secondWordDisplay.text = clickedLetters.toString()
+                } else if (remainingChances == 1){
+                    thirdWordDisplay.text = clickedLetters.toString()
+                }
+
             }
         }
         println(clickedLetters)
@@ -62,20 +74,49 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    fun checkGuess(guess: StringBuilder) : String {
-        Log.d("MainActivity", "Random Four-Letter Word: $wordToGuess")
-        for (i in 0..3) {
-            if (guess[i] == wordToGuess[i]) {
-                answer += "O"
+    fun checkGuess(guess: StringBuilder): CharSequence {
+        val builder = SpannableStringBuilder()
+
+        for (i in 0 until maxLetters) {
+            val guessedLetter = guess[i]
+            val correctLetter = wordToGuess[i]
+
+            val spannable = SpannableStringBuilder(guessedLetter.toString())
+
+            if (guessedLetter == correctLetter) {
+                spannable.setSpan(
+                    ForegroundColorSpan(Color.GREEN),
+                    0, 1,
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+            } else if (correctLetter in guess.toString()) {
+                spannable.setSpan(
+                    ForegroundColorSpan(Color.YELLOW),
+                    0, 1,
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+            } else {
+                spannable.setSpan(
+                    ForegroundColorSpan(Color.RED),
+                    0, 1,
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
             }
-            else if (guess[i] in wordToGuess) {
-                answer += "+"
-            }
-            else {
-                answer += "X"
-            }
+
+            builder.append(spannable)
         }
-        return answer
+
+        if(remainingChances == 3){
+            firstWordDisplay.text = builder
+        }else if(remainingChances == 2){
+            secondWordDisplay.text = builder
+        } else if(remainingChances == 1) {
+            thirdWordDisplay.text = builder
+        }
+
+        return builder
     }
+
+
 
 }
