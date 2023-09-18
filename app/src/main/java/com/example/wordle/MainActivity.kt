2bfae0f2b78d0f75518521c22090b56d3db25f7f
Norsang Nyandak <TNorsang>
2023-09-18 17:45:16 -0400

    Able to give 3 chances

--- app/src/main/java/com/example/wordle/MainActivity.kt
+++ app/src/main/java/com/example/wordle/MainActivity.kt
@@ -8,56 +8,74 @@ import android.widget.Button
 import android.widget.TextView
 
 import com.example.wordle.FourLetterWordLists
-
 class MainActivity : AppCompatActivity() {
     private lateinit var firstWordDisplay: TextView
-    private val clickedLetters = StringBuilder()
+    private lateinit var secondWordDisplay: TextView
+    private lateinit var thirdWordDisplay: TextView
+
+    private val fourLetterWordsList = FourLetterWordLists
+    private val wordToGuess = fourLetterWordsList.getRandomFourLetterWord()
+    private var answer : String = ""
+
     private val maxLetters = 4
+    private var clickedLetters = StringBuilder()
+    private var remainingChances = 3 // Set the initial number of chances
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         firstWordDisplay = findViewById(R.id.firstWordDisplay)
+        secondWordDisplay = findViewById(R.id.secondWordDisplay)
+        thirdWordDisplay = findViewById(R.id.thirdWordDisplay)
 
+        val guessButton = findViewById<Button>(R.id.guessButton)
+
+        guessButton.setOnClickListener{
+            if (remainingChances > 0) {
+                answer = checkGuess(clickedLetters)
+                Log.d("MainActivity", "Answer: $answer")
+                remainingChances-- // Decrease the remaining chances
+                clickedLetters.setLength(0)
+                Log.d("MainActivity", "Clicked Letters Should be empty: $clickedLetters")
+            }
+            if (remainingChances == 0) {
+                // Disable the button when chances are used up
+                guessButton.isEnabled = false
+            }
+
+        }
     }
+
     fun onKeyClick(view: View) : StringBuilder {
         if (view is Button) {
 
-            val letterClicked = view.text.toString()
+            var letterClicked = view.text.toString()
 
-            if(clickedLetters.length < maxLetters){
+            if (clickedLetters.length < maxLetters){
                 clickedLetters.append(letterClicked)
                 firstWordDisplay.text = clickedLetters.toString()
-
-                val answer = checkGuess(clickedLetters)
-                Log.d("Main Activity","Answer:$answer")
-
             }
         }
-
+        println(clickedLetters)
         return clickedLetters
     }
 
-    fun checkGuess(guess: StringBuilder) : String {
-        val fourLetterWordsList = FourLetterWordLists
-        val wordToGuess = fourLetterWordsList.getRandomFourLetterWord()
 
+    fun checkGuess(guess: StringBuilder) : String {
         Log.d("MainActivity", "Random Four-Letter Word: $wordToGuess")
-        var result = ""
         for (i in 0..3) {
             if (guess[i] == wordToGuess[i]) {
-                result += "O"
+                answer += "O"
             }
             else if (guess[i] in wordToGuess) {
-                result += "+"
+                answer += "+"
             }
             else {
-                result += "X"
+                answer += "X"
             }
         }
-        return result
+        return answer
     }
-
 
 }
